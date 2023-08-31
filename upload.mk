947c97f0bc824654ff4eab9300041d0c46bcdd00
wang-bin <wbsecg1@gmail.com>
2023-08-31 22:24:16 +0800

    ci:  do not upload zip to sf
    zip was used by cocoapods, now replaced by tar.xz

--- upload.mk
+++ upload.mk
@@ -1,10 +1,6 @@
-all: apple_txz apple_zip mac_txz mac_zip
+all: apple_txz mac_txz
 apple_txz:
 	sshpass -p $$SF_PW scp -o StrictHostKeyChecking=no mdk-sdk-apple.tar.xz $${SF_USER}@frs.sourceforge.net:/home/frs/project/mdk-sdk/nightly/
-apple_zip:
-	sshpass -p $$SF_PW scp -o StrictHostKeyChecking=no mdk-sdk-apple.zip $${SF_USER}@frs.sourceforge.net:/home/frs/project/mdk-sdk/nightly/
 mac_txz:
 	sshpass -p $${SF_PW} scp -o StrictHostKeyChecking=no mdk-sdk-macOS.tar.xz $${SF_USER}@frs.sourceforge.net:/home/frs/project/mdk-sdk/nightly/
-mac_zip:
-	sshpass -p $${SF_PW} scp -o StrictHostKeyChecking=no mdk-sdk-macOS.zip $${SF_USER}@frs.sourceforge.net:/home/frs/project/mdk-sdk/nightly/
 .PHONY: all
