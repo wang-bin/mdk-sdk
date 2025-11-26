1853d377fced82fbda73bb27c470178e613fa316
wangbin <wbsecg1@gmail.com>
2025-11-26 13:24:41 +0800

    ci: win pkg rename

--- upload.mk
+++ upload.mk
@@ -13,13 +13,18 @@ tvos_txz:
 visionos_txz:
 	[ -f mdk-sdk-visionOS.tar.xz ] && sshpass -p $${SF_PW} scp -o StrictHostKeyChecking=no mdk-sdk-visionOS.tar.xz $${SF_USER}@frs.sourceforge.net:/home/frs/project/mdk-sdk/nightly/ || echo no mdk-sdk-visionOS.tar.xz
 upload_win_ltl:
+	[ -f mdk-sdk-windows-desktop-vs2022-ltl.7z ] && sshpass -p $${SF_PW} scp -o StrictHostKeyChecking=no mdk-sdk-windows-desktop-vs2022-ltl.7z $${SF_USER}@frs.sourceforge.net:/home/frs/project/mdk-sdk/nightly/mdk-sdk-windows-ltl.7z || echo no mdk-sdk-windows-desktop-vs2022-ltl.7z
 	[ -f mdk-sdk-windows-desktop-vs2022-ltl.7z ] && sshpass -p $${SF_PW} scp -o StrictHostKeyChecking=no mdk-sdk-windows-desktop-vs2022-ltl.7z $${SF_USER}@frs.sourceforge.net:/home/frs/project/mdk-sdk/nightly/ || echo no mdk-sdk-windows-desktop-vs2022-ltl.7z
 upload_win:
+	[ -f mdk-sdk-windows-desktop-vs2022.7z ] && sshpass -p $${SF_PW} scp -o StrictHostKeyChecking=no mdk-sdk-windows-desktop-vs2022.7z $${SF_USER}@frs.sourceforge.net:/home/frs/project/mdk-sdk/nightly/mdk-sdk-windows.7z || echo no mdk-sdk-windows-desktop-vs2022.7z
+	[ -f mdk-sdk-windows-desktop-vs2022.7z.md5 ] && sshpass -p $${SF_PW} scp -o StrictHostKeyChecking=no mdk-sdk-windows-desktop-vs2022.7z.md5 $${SF_USER}@frs.sourceforge.net:/home/frs/project/mdk-sdk/nightly/mdk-sdk-windows.7z.md5 || echo no mdk-sdk-windows-desktop-vs2022.7z.md5
 	[ -f mdk-sdk-windows-desktop-vs2022.7z ] && sshpass -p $${SF_PW} scp -o StrictHostKeyChecking=no mdk-sdk-windows-desktop-vs2022.7z* $${SF_USER}@frs.sourceforge.net:/home/frs/project/mdk-sdk/nightly/ || echo no mdk-sdk-windows-desktop-vs2022.7z
 upload_win64:
+	[ -f mdk-sdk-windows-desktop-vs2022-x64.7z ] && sshpass -p $${SF_PW} scp -o StrictHostKeyChecking=no mdk-sdk-windows-desktop-vs2022-x64.7z $${SF_USER}@frs.sourceforge.net:/home/frs/project/mdk-sdk/nightly/mdk-sdk-windows-x64.7z || echo no mdk-sdk-windows-desktop-vs2022-x64.7z
+	[ -f mdk-sdk-windows-desktop-vs2022-x64.7z.md5 ] && sshpass -p $${SF_PW} scp -o StrictHostKeyChecking=no mdk-sdk-windows-desktop-vs2022-x64.7z.md5 $${SF_USER}@frs.sourceforge.net:/home/frs/project/mdk-sdk/nightly/mdk-sdk-windows-x64.7z.md5 || echo no mdk-sdk-windows-desktop-vs2022-x64.7z.md5
 	[ -f mdk-sdk-windows-desktop-vs2022-x64.7z ] && sshpass -p $${SF_PW} scp -o StrictHostKeyChecking=no mdk-sdk-windows-desktop-vs2022-x64.7z* $${SF_USER}@frs.sourceforge.net:/home/frs/project/mdk-sdk/nightly/ || echo no mdk-sdk-windows-desktop-vs2022-x64.7z
 upload_uwp:
-	[ -f mdk-sdk-uwp-vs2022.7z ] && sshpass -p $${SF_PW} scp -o StrictHostKeyChecking=no mdk-sdk-uwp-vs2022.7z $${SF_USER}@frs.sourceforge.net:/home/frs/project/mdk-sdk/nightly/ || echo no mdk-sdk-uwp-vs2022.7z
+	[ -f mdk-sdk-uwp.7z ] && sshpass -p $${SF_PW} scp -o StrictHostKeyChecking=no mdk-sdk-uwp.7z $${SF_USER}@frs.sourceforge.net:/home/frs/project/mdk-sdk/nightly/ || echo no mdk-sdk-uwp.7z
 upload_nupkg:
 	[ -f mdk-vs2022.nupkg ] && sshpass -p $${SF_PW} scp -o StrictHostKeyChecking=no mdk-vs2022.nupkg $${SF_USER}@frs.sourceforge.net:/home/frs/project/mdk-sdk/nightly/ || echo no mdk-vs2022.nupkg
 
